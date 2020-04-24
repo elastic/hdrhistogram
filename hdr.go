a87ec475793369fd41c63a11517acbdd1a517044
Andrew Wilkins <axwalk@gmail.com>
2020-04-24 12:00:33 +0800

    Use math/bits.Len64
    name                    old time/op    new time/op    delta
    HistogramRecordValue-8    10.8ns ± 4%     7.4ns ± 9%  -31.65%  (p=0.008 n=5+5)

--- hdr.go
+++ hdr.go
@@ -6,6 +6,7 @@ package hdrhistogram
 import (
 	"fmt"
 	"math"
+	"math/bits"
 )
 
 // A Bracket is a part of a cumulative distribution.
@@ -542,23 +543,5 @@ func (p *pIterator) next() bool {
 }
 
 func bitLen(x int64) (n int64) {
-	for ; x >= 0x8000; x >>= 16 {
-		n += 16
-	}
-	if x >= 0x80 {
-		x >>= 8
-		n += 8
-	}
-	if x >= 0x8 {
-		x >>= 4
-		n += 4
-	}
-	if x >= 0x2 {
-		x >>= 2
-		n += 2
-	}
-	if x >= 0x1 {
-		n++
-	}
-	return
+	return int64(bits.Len64(uint64(x)))
 }
